2c3a8ad448b516cbf55bf0a2ebe6241d0e32c90b
Robert <rquesadaqq@outlook.com>
2026-06-24 17:45:50 -0600

    Show dynamic greeting in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 
 <x-layout>
-	<section class="hero">
+	{{ $greeting }} {{ $name }}
+	<!-- <section class="hero">
 		<h1>Welcome to ISW811</h1>
 		<p>
 			Usa la barra superior para navegar entre Home, About y Contact.
 			Esta estructura comparte el mismo diseño en todas las páginas.
 		</p>
 		<a class="button" href="/about">Ir a About</a>
-	</section>
+	</section> -->
 </x-layout>
