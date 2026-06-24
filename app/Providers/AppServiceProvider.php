1e37dbac3f99060c058e8a707168113f2b97e6b4
Robert <rquesadaqq@outlook.com>
2026-06-23 22:50:32 -0600

    Add compiled view fallback

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $compiledViewsPath = config('view.compiled', storage_path('framework/views'));
+
+        if (! is_dir($compiledViewsPath) || ! is_writable($compiledViewsPath)) {
+            $fallbackPath = rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR)
+                .DIRECTORY_SEPARATOR.'laravel'
+                .DIRECTORY_SEPARATOR.md5(base_path())
+                .DIRECTORY_SEPARATOR.'views';
+
+            if (! is_dir($fallbackPath)) {
+                @mkdir($fallbackPath, 0777, true);
+            }
+
+            if (is_dir($fallbackPath) && is_writable($fallbackPath)) {
+                config(['view.compiled' => $fallbackPath]);
+            }
+        }
     }
 }
