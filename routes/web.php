e73c6428dd9cd028675bdfa6ad8f9660c0e8e61c
Robert <rquesadaqq@outlook.com>
2026-06-24 17:45:57 -0600

    Pass greeting data from the home route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::view('/about', 'about');
+
+// Route::view('/contact', 'contact');
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'greeting' => 'Hello World',
+        'name' => request('person', 'Guest'),
+    ]);
 });
-
-Route::view('/about', 'about');
-
-Route::view('/contact', 'contact');
