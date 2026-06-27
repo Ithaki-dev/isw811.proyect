3adc59d72661cbae1604fd3f68c78993b22a28d1
Robert <rquesadaqq@outlook.com>
2026-06-26 23:13:45 -0600

    add login button

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,4 +12,19 @@
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
     </button>
   </div>
+  @guest
+  <div class="flex-none">
+    <a href="/register" class="btn btn-ghost">Register</a>
+    <a href="/login" class="btn btn-ghost">Login</a>
+    </div>
+  @else
+  <div class="flex-none">
+    <form action="/logout" method="POST">
+      @csrf
+      @method('DELETE')
+      <button type="submit" class="btn btn-ghost">Logout</button>
+    </form>
+    </div>
+  @endguest
+
 </div>
