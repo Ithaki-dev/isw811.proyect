9402e5f5484f34b4eb360c9b0aa24715e6788c1a
Robert <rquesadaqq@outlook.com>
2026-06-27 16:30:17 -0600

    Update AppServiceProvider for authorization logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Access\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        
     }
 
     /**
@@ -19,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        Gate::define('view-admin', function ( $user) {
+            if ($user->id === 1) {
+                return Response::allow();
+            }
+            return Response::deny('You do not have permission to view the admin page.');
+        });
+
+
         $compiledViewsPath = config('view.compiled', storage_path('framework/views'));
 
         if (! is_dir($compiledViewsPath) || ! is_writable($compiledViewsPath)) {
