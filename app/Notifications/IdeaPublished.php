209a9aaa7510ecee43657fcdc56b818cf3e9ce03
Robert <rquesadaqq@outlook.com>
2026-06-28 17:36:02 -0600

    feat: implement notification system

---
+++ app/Notifications/IdeaPublished.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Idea;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class IdeaPublished extends Notification
+{
+    use Queueable;
+
+    public function __construct(public Idea $idea)
+    {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = url('/ideas/' . $this->idea->id);
+
+        return (new MailMessage)
+            ->greeting('Hello!')
+            ->line('You have a new idea published.')
+            ->action('Read it', $url)
+            ->line('Thank you for using our application!');
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [];
+    }
+}   
