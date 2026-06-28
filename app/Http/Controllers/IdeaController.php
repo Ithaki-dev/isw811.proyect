18aedaa7e21199d2d8a0cf7d5f736b224bf40459
Robert <rquesadaqq@outlook.com>
2026-06-27 18:59:47 -0600

    Update IdeaController to use Policies for authorization

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -6,6 +6,7 @@ use App\Models\Idea;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreIdeaRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class IdeaController extends Controller
 {
@@ -49,6 +50,8 @@ class IdeaController extends Controller
      */
     public function show(Idea $idea)
     {
+        Gate::authorize('update', $idea);
+
         return view('ideas.show', [
             'idea' => $idea,
         ]);
@@ -59,6 +62,8 @@ class IdeaController extends Controller
      */
     public function edit(Idea $idea)
     {
+        Gate::authorize('update', $idea);
+
         return view('ideas.edit', [
             'idea' => $idea,
         ]);
@@ -69,6 +74,7 @@ class IdeaController extends Controller
      */
     public function update(Request $request, Idea $idea)
     {
+        Gate::authorize('update', $idea);
         $attributes = $request->validate([
             'idea' => 'required',
         ]);
@@ -83,6 +89,7 @@ class IdeaController extends Controller
      */
     public function destroy(Idea $idea)
     {
+        Gate::authorize('update', $idea);
         $idea->delete();
 
         return redirect('/ideas');
