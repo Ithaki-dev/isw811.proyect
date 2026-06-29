cf2c8e26caaa0dc53f32e1e560bee39764a88f30
Robert <rquesadaqq@outlook.com>
2026-06-28 20:56:01 -0600

    Use default DaisyUI theme from CSS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,6 @@
 @props(['title' => 'ISW811'])
 <!DOCTYPE html>
-<html lang="es" data-theme="dracula">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
