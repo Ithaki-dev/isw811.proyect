eaf825bb9056efda9d5fba227b6fb604827eecee
Robert <rquesadaqq@outlook.com>
2026-06-29 18:37:46 -0600

    feat(auth): update user model relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -47,7 +48,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function ideas(): hasMany
+    public function ideas(): HasMany
     {
         return $this->hasMany(Idea::class);
     }
