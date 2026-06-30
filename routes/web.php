c7a4be54d072071d17556fec6b95f4b5c5a75800
Robert <rquesadaqq@outlook.com>
2026-06-30 15:28:59 -0600

    add login route name and idea show route

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,23 @@ Route::get('/', function () {
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('/register', [RegisterUserController::class, 'create']);
-    Route::post('/register', [RegisterUserController::class, 'store']);
+    Route::get('/register', [RegisterUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisterUserController::class, 'store'])->name('register');
 
-    Route::get('/login', [SessionController::class, 'create']);
-    Route::post('/login', [SessionController::class, 'store']);
+    Route::get('/login', [SessionController::class, 'create'])->name('login');
+    Route::post('/login', [SessionController::class, 'store'])->name('login');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/ideas', [IdeaController::class, 'index']);
-    Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
-    Route::post('/logout', [SessionController::class, 'destroy']);
+    Route::get('/ideas', [IdeaController::class, 'index'])->name('ideas.index');
+    Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
+
+    Route::get('/ideas/create', [IdeaController::class, 'create'])->name('ideas.create');
+    Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
+
+    Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
+    Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
+    Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+    
+    Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 });
