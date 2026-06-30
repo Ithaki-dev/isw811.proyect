a224c89d3d82d6b7d5729119144a907e2ef2a162
Robert <rquesadaqq@outlook.com>
2026-06-29 18:38:17 -0600

    build(vite): configure Vite development server

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: '192.168.56.10',
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
