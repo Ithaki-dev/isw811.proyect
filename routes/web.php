06690e088e82b8f817e991a1eb96fda3c0c447fc
Robert <rquesadaqq@outlook.com>
2026-06-30 21:58:05 -0600

    fix update route method from PUT to PATCH

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,11 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/ideas', [IdeaController::class, 'index'])->name('ideas.index');
-    Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
-
     Route::get('/ideas/create', [IdeaController::class, 'create'])->name('ideas.create');
     Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
-
+    Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
     Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
-    Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
+    Route::patch('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
     Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
-    
     Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 });
