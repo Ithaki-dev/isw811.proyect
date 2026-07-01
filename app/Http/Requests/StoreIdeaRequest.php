0dca9f73e084e2adb503cd7422cb2926cbcc23a0
Robert <rquesadaqq@outlook.com>
2026-06-30 21:58:05 -0600

    add validation rules and fix authorize in form requests

--- app/Http/Requests/StoreIdeaRequest.php
+++ app/Http/Requests/StoreIdeaRequest.php
@@ -7,23 +7,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreIdeaRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|min:3|max:255',
+            'description' => 'required|string|min:10',
+            'links' => 'nullable|string',
         ];
     }
 }
